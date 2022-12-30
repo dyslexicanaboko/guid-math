44d664f8edcdb183aeb527b316949103e4e577b3
Eli Hayon <dyslexicanaboko@gmail.com>
2022-12-30 01:56:10 -0500

    Create sequential guid.

---
+++ GuidMath.Lib/Services/SequentialGuidService.cs
@@ -0,0 +1,28 @@
+﻿namespace GuidMath.Lib.Services
+{
+    /// <summary>Alternative to rpcrt4.dll for sequential Guid generation.</summary>
+    public class SequentialGuidService
+    {
+        private readonly GuidMathService _math;
+        private readonly Guid _seedGuid;
+        private int _i;
+
+        public SequentialGuidService()
+        {
+            _seedGuid = Guid.NewGuid();
+
+            _math = new GuidMathService(_seedGuid);
+
+            _i = 0;
+        }
+
+        public Guid CreateSequentialGuid()
+        {
+            var nextGuid = _math.Add(_i);
+
+            _i++;
+
+            return nextGuid;
+        }
+    }
+}
